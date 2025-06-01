c8ffa518564d12977b0f391f7712e9a9aec709d4
Dragos Barbuta <barbuta.dragos12@gmail.com>
2025-06-01 20:34:04 +0200

    change redirect to correct url

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export const Footer = () => {
   return (
     <div className="px-5 py-3 border-t border-gray-100 bg-gray-50">
       <a
-        href="https://github.com/pulls/assigned"
+        href="https://github.com/pulls?q=is%3Apr+is%3Aopen+user-review-requested%3A%40me"
         target="_blank"
         rel="noopener noreferrer"
         className="text-xs text-blue-600 hover:text-blue-700 hover:underline transition-colors duration-200 font-medium"
